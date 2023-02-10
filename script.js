92a5391daf65885923f45f8892f31b6328c4ba2b
Sami <s.a.sami359359@gmail.com>
2023-02-10 20:16:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// // let categories = document.getElementsByClassName("categories")[0]
+// // categories.remove()
+// let home = document.getElementById("home")
+// let x = ()=>{
+//     window.location = "https://www.flipkart.com/"
+// }
+// home.addEventListener('click', x)
